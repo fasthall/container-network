852517b3fad92f140803ff726a6697307bece7e9
weitsung <weitsung@erlang.cs.ucsb.edu>
2016-06-02 11:01:45 -0700

    native bw test

---
+++ test/bw_native.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Running native bandwidth test $1"
+echo "native bandwidth test $1" > bw_native_$1
+iperf -c clojure.cs.ucsb.edu -r -t $2 -p 5012 > /dev/null &
+iperf -c ruby.cs.ucsb.edu -r -t $2 -p 5011 > /dev/null &
+iperf -c csharp.cs.ucsb.edu -r -t $2 -p 5013 > /dev/null &
+#iperf -c clojure.cs.ucsb.edu -r -t $2 -p 5013 >> bw_native_$1
+
+
